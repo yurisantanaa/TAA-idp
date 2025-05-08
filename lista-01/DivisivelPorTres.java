79364e8960bbf37f42daa40c0236114364960b04
yurisantanaa <yuri.santana@discente.ufg.br>
2025-05-08 08:28:23 -0300

    retira comentarios

--- lista-01/DivisivelPorTres.java
+++ lista-01/DivisivelPorTres.java
@@ -5,24 +5,18 @@ public class DivisivelPorTres {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            // Lê o número de dígitos
             int D = scanner.nextInt();
 
-            // Condição de parada
             if (D == -1) {
                 break;
             }
 
-            // Lê o número como uma string
             String N = scanner.next();
 
-            // Calcula a soma dos dígitos
             int soma = 0;
             for (int i = 0; i < N.length(); i++) {
                 soma += Character.getNumericValue(N.charAt(i));
             }
-
-            // Verifica se a soma é divisível por 3
             if (soma % 3 == 0) {
                 System.out.println(soma + " sim");
             } else {
